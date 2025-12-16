0d38bf0de615b12ac772d97a2fe0402209780852
Abinya muthu <abinayamuthu111@gmail.com>
2025-12-16 23:53:35 +0530

    day2pp commit

---
+++ JavaPracticesprograms/src/areacalculation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class areacalculation {
+
+	public static void main(String[] args) {
+		//accept the length and width
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the length of the area : ");
+		int length = sc.nextInt();
+		System.out.println("Enter the width of the area : ");
+		int width = sc.nextInt();
+		//process
+		int Area = length * width;
+		System.out.println("This is the total area of rectangle : "+Area);
+ sc.close();
+	}
+
+}
