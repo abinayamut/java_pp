e0cd0f83fccf21b48c90297845598c9612935d94
Abinya muthu <abinayamuthu111@gmail.com>
2025-12-27 21:15:36 +0530

    studd commit

---
+++ JavaPracticesprograms/src/loginvalidation.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class loginvalidation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your User name :");
+		String username = sc.next();
+		
+
+	}
+
+}
---
+++ JavaPracticesprograms/src/studentdetailsdisplay.java
@@ -0,0 +1,15 @@
+
+public class studentdetailsdisplay {
+	int id;
+	String name;
+	
+
+	public static void main(String[] args) {
+		studentdetailsdisplay s1 = new studentdetailsdisplay();
+		s1.id = 1002;
+		s1.name = "Abinaya";
+		 System.out.println(s1.id);
+		 System.out.println(s1.name);
+	}
+
+}
